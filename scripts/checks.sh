2def48c6198411ac401b0edfa5b08587625bffae
Vyacheslav Vershinin <vyacheslav.vershinin@acronis.com>
2018-11-07 16:27:05 +0300

    added pic into checks

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,5 +2,5 @@
 files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
-./tools/polly/bin/polly --reconfig --toolchain sanitize-address-cxx17 --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
-./tools/polly/bin/polly --reconfig --toolchain sanitize-thread-cxx17  --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
+./tools/polly/bin/polly --reconfig --toolchain sanitize-address-cxx17-pic --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
+./tools/polly/bin/polly --reconfig --toolchain sanitize-thread-cxx17-pic  --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
