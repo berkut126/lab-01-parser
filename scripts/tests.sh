ff7fa128f3f91de55240422d748b24ae0d8ed21e
drewxa <a.borodin@bmstu.ru>
2018-11-12 15:55:01 +0300

    Добавление покрытия кода в Github Checks (#3)
    Added coverage repoting into commit message

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,3 +4,9 @@ export CTEST_OUTPUT_ON_FAILURE=true
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 gcovr -r  .
+
+REPORT_DATA=$(gcovr -r  . | base64 | tr -d '\n')
+POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$TRAVIS_REPO_SLUG\", \"head_branch\": \"$TRAVIS_BRANCH\", \"head_sha\": \"$TRAVIS_COMMIT\"}"
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+curl -H "Content-Type: application/json" -d "$POST_DATA" -X POST http://borodin.dev.bmstu.cloud/api/coverage/
+fi
