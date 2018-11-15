2fb7019d112e1785545b2bbfa9f5e89e6b983578
rusdevops <rusdevops@gmail.com>
2018-11-15 14:17:53 +0300

    fixed continue of script run when error

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 export CTEST_OUTPUT_ON_FAILURE=true
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --test --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
 ./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
