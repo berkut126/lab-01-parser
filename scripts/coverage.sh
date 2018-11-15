7e3cf697e30a9685bdbd82f600dc1b79657ab957
rusdevops <rusdevops@gmail.com>
2018-11-15 19:12:15 +0300

    added separate job coverage

---
+++ scripts/coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --test --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+gcovr -r  .
+mkdir _coverage && find . -name "*.gcov" -exec mv -t _coverage {} \+
+
+REPORT_DATA=$(gcovr -r  . | base64 | tr -d '\n')
+POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$TRAVIS_REPO_SLUG\", \"head_branch\": \"$TRAVIS_BRANCH\", \"head_sha\": \"$TRAVIS_COMMIT\"}"
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+curl -H "Content-Type: application/json" -d "$POST_DATA" -X POST http://borodin.dev.bmstu.cloud/api/coverage/
+fi
