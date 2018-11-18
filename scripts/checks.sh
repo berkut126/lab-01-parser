eca6df02b99526fa81464c1f9d4e96b22a102eee
rusdevops <rusdevops@gmail.com>
2018-11-18 07:01:08 +0000

    fixed santizer run

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -3,5 +3,6 @@ set -e
 files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
-./tools/polly/bin/polly --reconfig --toolchain sanitize-address-cxx17-pic --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
-./tools/polly/bin/polly --reconfig --toolchain sanitize-thread-cxx17-pic  --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
+export CTEST_OUTPUT_ON_FAILURE=true
+./tools/polly/bin/polly --test --reconfig --toolchain sanitize-address-cxx17-pic --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
+./tools/polly/bin/polly --test --reconfig --toolchain sanitize-thread-cxx17-pic  --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
