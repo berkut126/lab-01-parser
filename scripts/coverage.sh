36280926a1d069563b9ef16ce49040ea1659bc06
rusdevops <rusdevops@gmail.com>
2018-11-19 11:29:15 +0300

    added colorize output

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
-./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --test --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
-./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target gcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
-./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --target lcov --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold" BUILD_COVERAGE=ON
+export GTEST_COLOR=1
+export CTEST_OUTPUT_ON_FAILURE=true
+CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
+CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
+CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/gcc-7-cxx17-pic.cmake'"
+CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
+cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON 
+cmake --build _builds
+cmake --build _builds --target test
+cmake --build _builds --target gcov
+cmake --build _builds --target lcov
 gcovr -r  .
-#mkdir _coverage && find . -name "*.gcov" -exec mv -t _coverage {} \+
 
 REPORT_DATA=$(gcovr -r  . | base64 | tr -d '\n')
 POST_DATA="{\"report\": \"$REPORT_DATA\", \"slug\": \"$TRAVIS_REPO_SLUG\", \"head_branch\": \"$TRAVIS_BRANCH\", \"head_sha\": \"$TRAVIS_COMMIT\"}"
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
-export CTEST_OUTPUT_ON_FAILURE=true
-./tools/polly/bin/polly --reconfig --toolchain gcc-7-cxx17-pic --test --config-all Debug --fwd CMAKE_EXE_LINKER="-fuse-ld=gold"
+export GTEST_COLOR=1
+CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
+CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
+CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/gcc-7-cxx17-pic.cmake'"
+CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
+cmake -H. -B_builds $CMAKE_OPTS
+cmake --build _builds
+cmake --build _builds --target test -- ARGS="--verbose"
