463e7cec64143cafe18dc319a3491712524416e6
berkut126 <berkut126@gmail.com>
2019-11-11 03:28:57 +0300

    Test travis.

--- _builds/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rooted/Projects/C++/lab-01-parser
-# Build directory: /home/rooted/Projects/C++/lab-01-parser/_builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/rooted/Projects/C++/lab-01-parser/_builds/tests")
