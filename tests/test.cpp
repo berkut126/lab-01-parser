9664a47c78f4cff5ec786690b9c19498b4e5db15
berkut126 <berkut126@gmail.com>
2019-12-06 13:40:55 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -28,5 +28,5 @@ TEST(Example, EmptyTest) {
   ss << j2;
   EXPECT_EQ(
       ss.str(),
-      R"~({data:[{"ticker":"Si-9.15","id": 100024,"description":"Futures contract for USD/RUB"},{"ticker":"RTS-9.15","id":100027,"description":"Futures contract for index RTS"},{"ticker":"GAZP-9.15","id": 100050,"description":"Futures contract for GAZPROM shares"}]})~");
+      "{\"data\":[[\"ticker\",\"Si-9.15\",\"id\",100024,\"description\",\"Futures contract for USD/RUB\"],[\"ticker\",\"RTS-9.15\",\"id\",100027,\"description\",\"Futures contract for index RTS\"],[\"ticker\",\"RTS-9.15\",\"id\",100027,\"description\",\"Futures contract for index RTS\"]]}"
 }
